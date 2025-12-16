632aa80fc21e00c5dc35307ac6ded3a615c37e5b
gaddiellr <gddllrrol@gmail.com>
2025-12-16 00:01:10 -0600

    UPDATE - NEW

--- Astromega/Assets/Scripts/AstSpawner.cs
+++ Astromega/Assets/Scripts/AstSpawner.cs
@@ -17,14 +17,14 @@ public class AstSpawner : MonoBehaviour
     {
         if (boxArea == null)
         {
-            Debug.LogError("No asignaste el BoxCollider del �rea base.");
+            Debug.LogError("No asignaste el BoxCollider del  rea base.");
             return;
         }
 
-        // Obtener los l�mites del BoxCollider
+        // Obtener los l mites del BoxCollider
         Bounds bounds = boxArea.bounds;
 
-        // Crear una posici�n aleatoria dentro del box
+        // Crear una posici n aleatoria dentro del box
         Vector3 randomPos = new Vector3(
             Random.Range(bounds.min.x, bounds.max.x),
             Random.Range(bounds.min.y, bounds.max.y),
@@ -34,7 +34,6 @@ public class AstSpawner : MonoBehaviour
         Instantiate(prefab, randomPos, Quaternion.identity);
     }
 }
-*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -50,6 +49,37 @@ public class AstSpawner : MonoBehaviour
         {
             Vector3 randomSpawnPosition = new Vector3(Random.Range(0, 21), 100, Random.Range(0, 21));
             Instantiate(prefab, randomSpawnPosition, Quaternion.identity);
+        }
+    }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    private string[] equations = { "5x + 4 = 9", "12x + 1 = 49" };
+    private int[,] solutions = { {1, 5, 2, 7}, {4, 3, 6, 8} };
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int index = UnityEngine.Random.Range(0, equations.Length);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Vector3 pos = new Vector3(
+                    UnityEngine.Random.Range(0, 21),
+                    100,
+                    UnityEngine.Random.Range(0, 21)
+                );
+
+                Instantiate(prefabs[solutions[index, i]], pos, Quaternion.identity);
+            }
         }
     }
 }
