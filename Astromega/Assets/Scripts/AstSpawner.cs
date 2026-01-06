b5d4f5d1519ecff003608f29ae3ffb8aea0cf45d
gaddiellr <gddllrrol@gmail.com>
2026-01-06 17:00:06 -0600

    Update Spawn Position

--- Astromega/Assets/Scripts/AstSpawner.cs
+++ Astromega/Assets/Scripts/AstSpawner.cs
@@ -83,7 +83,6 @@ public class AstSpawner : MonoBehaviour
         }
     }
 }
-*/
 
 using System.Collections;
 using System.Collections.Generic;
@@ -125,5 +124,77 @@ public class AstSpawner : MonoBehaviour
                 }
             }
         }
+    }
+}
+*/
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    [SerializeField] private float spawnRadius = 1.15f;
+    [SerializeField] private LayerMask asteroidMask;
+
+    [SerializeField] private int a;
+    [SerializeField] private int b;
+    [SerializeField] private int r;
+
+    public int A => a;
+    public int B => b;
+    public int R => r;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            List<int> num = new() { 0,1,2,3,4,5,6,7,8,9 };
+
+            int x = Random.Range(0, 10);
+            a = Random.Range(-100, 101);
+            b = Random.Range(-50, 50);
+            r = x * a + b;
+
+            num.Remove(x);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Vector3 pos = GetFreePosition();
+
+                if (i == 1)
+                {
+                    Instantiate(prefabs[x], pos, Quaternion.identity);
+                }
+                else
+                {
+                    int notx = num[Random.Range(0, num.Count)];
+                    Instantiate(prefabs[notx], pos, Quaternion.identity);
+                    num.Remove(notx);
+                }
+            }
+        }
+    }
+
+    Vector3 GetFreePosition()
+    {
+        Vector3 pos = new Vector3(Random.Range(0, 21), 100, Random.Range(0, 21));
+        bool finish = IsPositionFree(pos);
+        while (!finish)
+        {
+            pos = new Vector3(Random.Range(0, 21), 100, Random.Range(0, 21));
+            finish = IsPositionFree(pos);
+        }
+        return pos;
+    }
+
+    bool IsPositionFree(Vector3 position)
+    {
+        return !Physics.CheckSphere(
+            position,
+            spawnRadius,
+            asteroidMask,
+            QueryTriggerInteraction.Ignore
+        );
     }
 }
