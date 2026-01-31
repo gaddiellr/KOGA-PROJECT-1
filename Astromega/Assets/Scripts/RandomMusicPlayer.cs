0125278a85b647f4a5b5f2a3a9f05cd5cf2d0792
MR.🥤🥤🥤 <kokaslocas123@gmail.com>
2026-01-31 13:58:27 -0600

    31_01_2026
    SoundTrack

---
+++ Astromega/Assets/Scripts/RandomMusicPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] soundtracks;
+
+    private int lastIndex = Random.Range(0, 3);
+
+    void Start()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+
+        PlayRandomTrack();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying && soundtracks.Length > 0)
+        {
+            PlayRandomTrack();
+        }
+    }
+
+    void PlayRandomTrack()
+    {
+        if (soundtracks.Length == 0) return;
+
+        int randomIndex;
+
+        do
+        {
+            randomIndex = Random.Range(0, soundtracks.Length);
+        }
+        while (randomIndex == lastIndex && soundtracks.Length > 1);
+
+        lastIndex = randomIndex;
+
+        audioSource.clip = soundtracks[randomIndex];
+        audioSource.Play();
+    }
+}
